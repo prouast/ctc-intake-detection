787d80cb4c4082f416b6bc94fbd0eeb67c101532
Philipp Rouast <philipp@rouast.com>
2020-03-10 15:15:06 +1100

    Adopt information available in paper draft

--- inert_heydarian_cnn_lstm.py
+++ inert_heydarian_cnn_lstm.py
@@ -8,33 +8,30 @@ class Model(tf.keras.Model):
     def __init__(self, num_classes, l2_lambda):
         super(Model, self).__init__()
         self.conv1d_1 = tf.keras.layers.Conv1D(
-            filters=64, kernel_size=1, padding='same',
+            filters=128, kernel_size=1, padding='same',
             activation=tf.nn.relu,
             kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
         self.conv1d_2 = tf.keras.layers.Conv1D(
-            filters=128, kernel_size=10, padding='same',
+            filters=128, kernel_size=3, padding='same',
             activation=tf.nn.relu,
             kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
         self.conv1d_3 = tf.keras.layers.Conv1D(
-            filters=256, kernel_size=10, padding='same',
+            filters=128, kernel_size=5, padding='same',
             activation=tf.nn.relu,
             kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
         self.conv1d_4 = tf.keras.layers.Conv1D(
-            filters=512, kernel_size=10, padding='same',
+            filters=128, kernel_size=7, padding='same',
             activation=tf.nn.relu,
             kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
         self.max_pool = tf.keras.layers.MaxPool1D(pool_size=2)
         self.dropout = tf.keras.layers.Dropout(rate=0.5)
-        self.dense_1 = tf.keras.layers.Dense(
-            units=64, activation=tf.nn.relu,
-            kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
         self.lstm_1 = tf.keras.layers.LSTM(
-            units=128, return_sequences=True,
+            units=64, return_sequences=True,
             kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
         self.lstm_2 = tf.keras.layers.LSTM(
-            units=256, return_sequences=True,
+            units=64, return_sequences=True,
             kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
-        self.dense_2 = tf.keras.layers.Dense(
+        self.dense_1 = tf.keras.layers.Dense(
             units=num_classes,
             kernel_regularizer=tf.keras.regularizers.l2(l2_lambda))
 
@@ -42,6 +39,7 @@ class Model(tf.keras.Model):
     def __call__(self, inputs, training=False):
         inputs = self.conv1d_1(inputs)
         inputs = self.dropout(inputs)
+        inputs = self.max_pool(inputs)
         inputs = self.conv1d_2(inputs)
         inputs = self.dropout(inputs)
         inputs = self.max_pool(inputs)
@@ -51,10 +49,8 @@ class Model(tf.keras.Model):
         inputs = self.conv1d_4(inputs)
         inputs = self.dropout(inputs)
         inputs = self.max_pool(inputs)
-        inputs = self.dense_1(inputs)
-        inputs = self.dropout(inputs)
         inputs = self.lstm_1(inputs)
         inputs = self.lstm_2(inputs)
-        inputs = self.dense_2(inputs)
+        inputs = self.dense_1(inputs)
         inputs = self.dropout(inputs)
         return inputs
